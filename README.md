38dee3e6df1d9bce8a034a19bcc1eb5735965ef4
DianaYYJeong <Diana.yy.Jeong@gmail.com>
2024-07-31 00:30:14 +0900

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+# Algorithm_Study
+---
+## [SWEA](https://github.com/EliteZer0/Algorithm_Study/tree/main/SWEA) (2024.07.26 ~)
+- ### **SSAFY 서울 10반 스터디 합류**
+
+    [1주차](https://github.com/EliteZer0/Algorithm_Study/tree/main/SWEA/W1) (2024.07.19 ~ 2024.07.25) // 미합류
+
+    [2주차](https://github.com/EliteZer0/Algorithm_Study/tree/main/SWEA/W2) (2024.07.26 ~ 2024.08.01)
---
+++ SWEA/W1/1926/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n + 1; i++) {
+
+            int hundreds = i / 100;
+            int tens = i / 10;
+            int unit = i % 10;
+
+            if (hundreds == 3 || hundreds == 6 || hundreds == 9) {
+                System.out.print("-");
+            }
+
+            if (tens == 3 || tens == 6 || tens == 9) {
+                System.out.print("-");
+            }
+
+            if (unit == 3 || unit == 6 || unit == 9) {
+                System.out.print("-");
+            }
+
+            if (hundreds != 3 && hundreds != 6 && hundreds != 9 && tens != 3 && tens != 6 && tens != 9 && unit != 3
+                    && unit != 6 && unit != 9) {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+
+        }
+    }
+}

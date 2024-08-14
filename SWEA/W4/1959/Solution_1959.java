0ee8ab1986bfbc6c4559ea55bc85178073a4d2a5
DianaYYJeong <Diana.yy.Jeong@gmail.com>
2024-08-14 10:38:53 +0900

    다른 풀이법

--- SWEA/W4/1959/Solution_1959.java
+++ SWEA/W4/1959/Solution_1959.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//maxArr minArr로 받는게 더 빠르지 않을까 했는데 이게 메모리 1500kb 시간 5ms 더 적게걸림.
 public class Solution_1959 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

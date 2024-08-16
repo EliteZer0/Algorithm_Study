fc381557d47f88de8838cc72de82e5316f069540
DianaYYJeong <Diana.yy.Jeong@gmail.com>
2024-08-16 16:17:43 +0900

    폴더명 변경

--- IM_Practice/IM_Exercise/IM_Exercise_1.java
+++ IM_Practice/IM_Exercise/IM_Exercise_1.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 //엘사의 전등 스위치
-public class IM_practice1 {
+public class IM_Exercise_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());

ce6dc51ab8ead79d180ca39a8d8ecb5f537206f0
DianaYYJeong <Diana.yy.Jeong@gmail.com>
2024-08-29 09:24:03 +0900

    W6 제출

---
+++ SAFFY_Study/W6/BOJ_14225/Main_14225.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_14225 {
+	static int target;
+	static int[] data;
+	static boolean[] canMake;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		data = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			data[i] = Integer.parseInt(st.nextToken());
+		}
+
+		canMake = new boolean[2000000];
+		
+		powerset(0, 0);
+		for (int i = 1; i < canMake.length; i++) {
+			if(!canMake[i]) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+	static void powerset(int idx, int sum) {
+		if (idx == data.length) {
+			canMake[sum] = true;
+			return;
+		}
+		powerset(idx + 1, sum + data[idx]);
+		powerset(idx + 1, sum);
+	}
+}
